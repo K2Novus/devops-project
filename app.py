648c1f73bb2eb7a140dad92bbcffad407b4b980b
Ubuntu <ubuntu@ip-172-31-95-203.ec2.internal>
2026-05-06 23:15:43 +0000

    test ci/cd pipeline

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+@app.route("/")
+def home():
+    return "CI/CD IS WORKING íº€"from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps Pipeline Running"
+    return "CI/CD IS WORKING "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
