7cc5c02e78ccc4bf171c3258843b49b7e61748d2
Ubuntu <ubuntu@ip-172-31-95-203.ec2.internal>
2026-05-06 23:19:34 +0000

    trigger ci cd pipeline

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-@app.route("/")
-def home():
-    return "CI/CD IS WORKING íº€"from flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD IS WORKING "
+    return "CI/CD IS WORKING íº€"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+    
